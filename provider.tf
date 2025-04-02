576b55c9c81060d3aacaac88ed45bd81143b9f54
kolosovpetro <kolosovp94@gmail.com>
2025-04-02 22:28:09 +0200

    provider

--- provider.tf
+++ provider.tf
@@ -4,5 +4,5 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false
     }
   }
-  subscription_id = var.subscription_id
+  subscription_id = "1b08b9a2-ac6d-4b86-8a2f-8fef552c8371"
 }
